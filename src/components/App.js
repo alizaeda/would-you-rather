da4993f3c5d3a695cf79bcc37e8aede9cef2e1c5
Ali Eslam <alizaeda92@gmail.com>
2021-09-16 10:53:16 +0200

    Starter files with Material-UI

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Avatar } from "@material-ui/core";
+
+class App extends Component {
+  render() {
+    return <div>Starter Files</div>;
+  }
+}
+
+export default App;
